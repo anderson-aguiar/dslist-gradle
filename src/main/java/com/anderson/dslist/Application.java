ee5cf58f96017e34d19493011680bea90751ba0b
Anderson Martins <anderson.aguiar@outlook.com.br>
2025-08-20 15:14:13 -0300

    Game seed

--- src/main/java/com/anderson/dslist/Application.java
+++ src/main/java/com/anderson/dslist/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DslistApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DslistApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
